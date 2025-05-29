c709e0da579430fde09d19ef0f5563e1764f7c2a
arijitkayal-gm <arijitkayal686@gmail.com>
2025-05-29 18:08:40 +0530

    Fixing Logo Issues

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import TextLogo from '../assets/Text.png';
 
 const Navbar = (props) => {
     return (
         <nav className={`navbar ${props.mode === 'dark' ? 'dark-nav' : 'light-nav'}`}>
             <div className="navbar-container">
                 <a href="#" className="navbar-title">
-                    <span><img src='./src/assets/Text.png' alt="icon" className='nav-logo' /></span>
+                    <span><img src={TextLogo} alt="icon" className='nav-logo' /></span>
                     <span className='nav-logo-title'>{props.title || 'Set Title Here'}</span>
                 </a>
 
